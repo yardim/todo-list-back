5b411a3691a8dd786224b1e6b2231271cdc82d57
yardim <yaroshenko@bluefountainmedia.com>
2018-07-21 21:15:28 +0300

    return id in todo-list creation response

--- services/todoLists.service.js
+++ services/todoLists.service.js
@@ -4,12 +4,23 @@ const _ = require('lodash');
 class TodoListsService {
   createList(params, userID) {
     params.userID = userID;
-    return new TodoListsModel(params).save();
+    return new TodoListsModel(params).save()
+      .then(todoList => _.pick(todoList, ['name', '_id']))
+      .then(todoList => {
+        const clearList = {
+          ...todoList,
+          id: todoList._id
+        }
+        delete clearList.id
+
+        return clearList;
+      });
   }
 
   getTodoLists(userID) {
     return TodoListsModel.find({ userID }).then(todoLists => {
       return todoLists.map(list => ({
+        id: list._id,
         name: list.name,
         todos: list.todos
       }));
